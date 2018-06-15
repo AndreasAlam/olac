b606505772a5a3ddc68e61515c66fed1e78aa0f4
Susanne Groothuis <s.groothuis@amsterdam.nl>
2018-06-15 16:21:30 +0200

    Created main function that takes a GENERATOR and  MODEL to show model accuracy over time

--- olac/perceptron.py
+++ olac/perceptron.py
@@ -39,84 +39,110 @@ def generate_data(n_samples):
     return X, yhat
 
 
-def sig(z):
-    return 1 / (1 + np.exp(-z))
+class Perceptron(object):
 
+    def __init__(self):
+        self.weights = []
 
-def dsig_dz(z):
-    return sig(z) * (1 - sig(z))
+    def sig(self, z):
+        return 1 / (1 + np.exp(-z))
 
+    def dsig_dz(self, z):
+        return self.sig(z) * (1 - self.sig(z))
 
-def J(y, yhat):
-    eps = 1e-8
-    return -(yhat*np.log(y+eps) + (1-yhat)*np.log(1-y+eps))
+    def J(self, y, yhat):
+        eps = 1e-8
+        return -(yhat*np.log(y+eps) + (1-yhat)*np.log(1-y+eps))
 
+    def dJ_dy(self, y, yhat):
+        eps = 1e-8
+        return (1-yhat)/(1-y+eps) - yhat/(y+eps)
 
-def dJ_dy(y, yhat):
-    eps = 1e-8
-    return (1-yhat)/(1-y+eps) - yhat/(y+eps)
+    def relu(self, z):
+        return np.where(z > 0, z, 0)
 
+    def drelu_dz(self, z):
+        return np.where(z > 0, 1, 0)
 
-def relu(z):
-    return np.where(z > 0, z, 0)
+    def backwardJ(self, x0, w1, b1, w2, b2, y, yhat, alpha):
+        # quantities
+        z1 = np.dot(x0, w1) + b1.T
+        x1 = self.relu(z1)
+        z2 = np.dot(x1, w2) + b2.T
+        # y = sig(z2)
 
+        delta2 = self.dJ_dy(y, yhat) * self.dsig_dz(z2)
+        delta1 = np.matmul(w2, delta2) * self.drelu_dz(z1).T
 
-def drelu_dz(z):
-    return np.where(z > 0, 1, 0)
+        w2 -= alpha * np.multiply(delta2, x1).T
+        w1 -= alpha * np.multiply(delta1, x0).T
 
+        b2 -= alpha * delta2
+        b1 -= alpha * delta1
 
-def backwardJ(x0, w1, b1, w2, b2, y, yhat, alpha):
-    # quantities
-    z1 = np.dot(x0, w1) + b1.T
-    x1 = relu(z1)
-    z2 = np.dot(x1, w2) + b2.T
-    # y = sig(z2)
+        return w1, b1, w2, b2
 
-    delta2 = dJ_dy(y, yhat) * dsig_dz(z2)
-    delta1 = np.matmul(w2, delta2) * drelu_dz(z1).T
+    def predict(self, x0, w1, b1, w2, b2):
+        x1 = self.relu(np.dot(x0, w1) + b1.T)  # output of hidden layer
+        return self.sig(np.dot(x1, w2) + b2.T)  # output of output layer
 
-    w2 -= alpha * np.multiply(delta2, x1).T
-    w1 -= alpha * np.multiply(delta1, x0).T
+    def train_network(self, X, labels, step, nr_epochs, n_hidden):
+        # implementation of pseudocode
+        # initialize weights
+        # n_samples = X.shape[0]
+        n_input = X.shape[1]
+        n_out = 1
+        costs = np.zeros(shape=(nr_epochs, 1))
+        y_hat_save = np.zeros(shape=(nr_epochs, len(labels)))
+    #    X = input data
+        w1 = np.random.normal(0, 0.1, size=(n_input, n_hidden))
+        w2 = np.random.normal(0, 0.1, size=(n_hidden, n_out))
 
-    b2 -= alpha * delta2
-    b1 -= alpha * delta1
+        b1 = np.random.normal(0, 0.1, size=(n_hidden, 1))
+        b2 = np.random.normal(0, 0.1, size=(n_out, 1))
 
-    return w1, b1, w2, b2
+        for epoch in range(nr_epochs):
+            for i, row in enumerate(X):
+                y_pred = self.predict(row, w1, b1, w2, b2)
+                w1, b1, w2, b2 = self.backwardJ(row, w1, b1, w2, b2, y_pred, labels[i], step)
 
+            curr_pred = self.predict(X, w1, b1, w2, b2)
+            costs[epoch] = np.mean(self.J(curr_pred, labels))
+            y_hat_save[epoch] = np.squeeze(curr_pred.round())
 
-def prediction(x0, w1, b1, w2, b2):
-    x1 = relu(np.dot(x0, w1) + b1.T)  # output of hidden layer
-    return sig(np.dot(x1, w2) + b2.T)  # output of output layer
+            if ((epoch % 10) == 0) or (epoch == (nr_epochs - 1)):
+                # print(curr_pred.round())
+                # print((labels == curr_pred.round()).sum())
+                accuracy = np.mean(np.equal(curr_pred[:, 0].round(), labels[:, 0]))  # current accuracy on entire set
+                print('Training accuracy after epoch {}: {:.4%}'.format(epoch, accuracy))
 
+        return w1, b1, w2, b2, costs, y_hat_save
 
-def train_network(X, labels, step, nr_epochs, n_hidden):
-    # implementation of pseudocode
-    # initialize weights
-    # n_samples = X.shape[0]
-    n_input = X.shape[1]
-    n_out = 1
-    costs = np.zeros(shape=(nr_epochs, 1))
-    y_hat_save = np.zeros(shape=(nr_epochs, len(labels)))
-#    X = input data
-    w1 = np.random.normal(0, 0.1, size=(n_input, n_hidden))
-    w2 = np.random.normal(0, 0.1, size=(n_hidden, n_out))
+    def fit_model(self, X, labels, epochs, step, n_hidden):
+        # implementation of pseudocode
+        # initialize weights
+        # n_samples = X.shape[0]
+        n_input = X.shape[1]
+        n_out = 1
 
-    b1 = np.random.normal(0, 0.1, size=(n_hidden, 1))
-    b2 = np.random.normal(0, 0.1, size=(n_out, 1))
+    #    X = input data
+        w1 = np.random.normal(0, 0.1, size=(n_input, n_hidden))
+        w2 = np.random.normal(0, 0.1, size=(n_hidden, n_out))
 
-    for epoch in range(nr_epochs):
-        for i, row in enumerate(X):
-            y_pred = prediction(row, w1, b1, w2, b2)
-            w1, b1, w2, b2 = backwardJ(row, w1, b1, w2, b2, y_pred, labels[i], step)
+        b1 = np.random.normal(0, 0.1, size=(n_hidden, 1))
+        b2 = np.random.normal(0, 0.1, size=(n_out, 1))
 
-        curr_pred = prediction(X, w1, b1, w2, b2)
-        costs[epoch] = np.mean(J(curr_pred, labels))
-        y_hat_save[epoch] = np.squeeze(curr_pred.round())
+        for epoch in range(epochs):
+            for i, row in enumerate(X):
+                y_pred = self.predict(row, w1, b1, w2, b2)
+                w1, b1, w2, b2 = self.backwardJ(row, w1, b1, w2, b2, y_pred, labels[i], step)
 
-        if ((epoch % 10) == 0) or (epoch == (nr_epochs - 1)):
-            # print(curr_pred.round())
-            # print((labels == curr_pred.round()).sum())
-            accuracy = np.mean(np.equal(curr_pred[:, 0].round(), labels[:, 0]))  # current accuracy on entire set
-            print('Training accuracy after epoch {}: {:.4%}'.format(epoch, accuracy))
+            # curr_pred = self.predict(X, w1, b1, w2, b2)
 
-    return w1, b1, w2, b2, costs, y_hat_save
+            # if ((epoch % 10) == 0) or (epoch == (epochs - 1)):
+            #     # print(curr_pred.round())
+            #     # print((labels == curr_pred.round()).sum())
+            #     accuracy = np.mean(np.equal(curr_pred[:, 0].round(), labels[:, 0]))  # current accuracy on entire set
+            #     print('Training accuracy after epoch {}: {:.4%}'.format(epoch, accuracy))
+
+        return w1, b1, w2, b2
--- olac/vis.py
+++ olac/vis.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import numpy as np
 import time
 
-import olac.perceptron as pc
+from olac.perceptron import Perceptron as pc
 
 
 def demo_plot():
@@ -85,13 +85,13 @@ class GetNewMetric:
 
         try:
             predictions = model.predict(data)
-        except AttributeError:
+        except TypeError:
             w1, b1, w2, b2 = weights
-            predictions = pc.prediction(data, w1, b1, w2, b2)
+            predictions = model.predict(data, w1, b1, w2, b2)
+        
+        accuracy = np.equal(predictions.round(), labels).mean()
 
-        accuracy = np.equal(predictions[:, 0].round(), labels[:, 0]).mean()
-
-        return predictions[:, 0].round(), accuracy
+        return predictions.round(), accuracy
 
     @staticmethod
     def get_new_precision(data, labels, model=None, weights=None):
@@ -162,26 +162,65 @@ class GetNewMetric:
         return predictions[:, 0].round(), TP/(TP+FN)
 
 
-def main_function(data, MODEL, GENERATOR, iterations, metric, weights=None, **kwargs):
+def main_function(MODEL, GENERATOR, iterations, metric, weights=None, window=20, **kwargs):
     """
 
     """
+    data = np.array(list(GENERATOR(steps=iterations, **kwargs)))
+    X = data[:, :2]
+    labels = data[:, -1]
+    wdw = int(iterations*0.1)
+
+    xlim = (min(X[:, 0]) + min(X[:, 0])/40, max(X[:, 0])+max(X[:, 0])/40)
+    ylim = (min(X[:, 1]) + min(X[:, 1])/40, max(X[:, 1])+max(X[:, 1])/40)
     if weights:
-        # dont train model\
-
+        # dont train model
+        pass
     else:
         # train model
-        GETFIRST_DATA_TO_TRAIN_MODEL
+        try:
+            weights = MODEL.fit(X[:wdw], labels[:wdw], epochs=200, batch_size=32)
+        except AttributeError:
+            # model = MODEL
+            weights = MODEL.fit_model(X[:wdw], labels[:wdw], epochs=500, step=0.001, n_hidden=3)
+
         print("Training model")
         # get weights
 
-    for i, x in enumerate(generator()):
+    metric_in_time = []
+    for i in np.arange(1, iterations, int(window/4)):
 
-        data_point = whatever
-        data = whatever
+        X_window = X[i:window+i]
+        label_window = labels[i:window+i]
 
-        metric_in_time = []
-        new_pred, metric = getattr(GetNewMetric, 'get_new_' + metric)
-        metric_in_time.append(metric)
+        new_pred, metric_value = getattr(GetNewMetric, 'get_new_' + metric)(X_window, label_window, MODEL, weights)
+        metric_in_time.append(metric_value)
 
+        plt.figure(figsize=(20, 10))
+        plt.subplot(121)
+        plt.plot(np.arange(0, len(metric_in_time)), metric_in_time)
+        plt.xlim(0, int(len(np.arange(1, iterations, int(window/2)))))
+        plt.ylim(0, 1.1)
+        plt.title(metric+'{:.4%}'.format(metric_value))
 
+        plt.subplot(122)
+        x1 = np.linspace(xlim[0], xlim[1], 100)
+        x2 = np.linspace(ylim[0], ylim[1], 100)
+        fun_map = np.empty((x1.size, x2.size))
+        for nn, ii in enumerate(x1):
+            for mm, jj in enumerate(x2):
+                w1, b1, w2, b2 = weights
+                fun_map[mm, nn] = MODEL.predict([ii, jj], w1, b1, w2, b2)
+        plt.imshow(fun_map, extent=[x1.min(), x1.max(), x2.min(), x2.max()],
+               vmin=0, vmax=1, aspect='auto', origin='lower')
+        plt.scatter(*X_window.T, color=[{0: 'w', 1: 'k', 2: 'b'}[n] for n in label_window.astype(int)], alpha=.3)
+        # plt.scatter(*X[new_pred == 0, :].T, color='w', alpha=.3)
+        plt.scatter(*X_window[np.not_equal(new_pred[:, 0], label_window), :].T, color='r')
+        plt.xlim(xlim)
+        plt.ylim(ylim)
+        # x1 = np.linspace(0, 1000, 100)
+        # x2 = np.linspace(0, 1000, 100)
+
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
+        plt.close()
