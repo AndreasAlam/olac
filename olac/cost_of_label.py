38655a0642d3367a56236f268aee0b2d88b47a54
bram2506 <bram2506@gmail.com>
2018-07-27 11:20:47 +0200

    removed the untitled empty notebooks from branch, improved functions which calculate the cost of investigating a data point or an array of datapoints

--- olac/cost_of_label.py
+++ olac/cost_of_label.py
@@ -1,7 +1,8 @@
 import numpy as np
+import pandas as pd
 
 
-def cost_of_label(data, decision, classification, data_type='point'):
+def cost_of_label(data, decision, data_type='point'):
     """
     This function gives back the cost of obtaining a label from the incoming data. It will check if the classification
     of the points one wants to investigate is correct and assesses a profit or loss on each result.
@@ -11,9 +12,8 @@ def cost_of_label(data, decision, classification, data_type='point'):
     Parameters
     ----------
     data : can be either a data point or a array of data points.
-    decision : bool or array, investigate a point True or False
-    classification : label of data point given by model.
-    data_type : is the incoming data a point or an array of data/batch
+    decision : bool or array of bools, investigate a point True or False
+    data_type : how does the data come in, either 'array' or 'point' like.
 
     Returns
     -------
@@ -21,32 +21,27 @@ def cost_of_label(data, decision, classification, data_type='point'):
 
     """
     if data_type == 'point':
-        if decision == 1:
-            # for pointlike input
-            cost = cost_investigation(data, classification)
-            return cost
-        else:
-            return 0.0
+        cost = cost_investigation(data, decision)
+        return cost
 
     elif data_type == 'array':
         # array like data input
-        df = pd.DataFrame(list(data), columns={'x', 'y', 'label'})
-        df['pred_label'] = classification
-        df_invest = df[decision == 1].copy()
-        df['cost'] = df_invest.apply(lambda x: cost_investigation(x[['x', 'y', 'label']], x['pred_label']), axis=1)
-        df.fillna(0, inplace=True)
+        df = pd.DataFrame(list(data))
+        df.columns = ['x', 'y', 'label']
+        df['decision'] = decision
+        df['cost'] = df.apply(lambda x: cost_investigation(x[['x', 'y', 'label']], x['decision']), axis=1)
 
         return np.array(df['cost'])
 
 
-def cost_investigation(data_point, predicted_label, salary=-1.0, fraud_label=1):
+def cost_investigation(data_point, decision, salary=-1.0, fraud_label=1):
     """
     Function that looks at the data is there fraud or not compared to what the model predicts.
     We want to obtain the labels, did we make a correct decission or not and the cost consequences are made
     Parameters
     ----------
     data_point : series or array like object that has as last entry the true label
-    predicted_label : classification label given by the model
+    decision : array or bool if we have investigated the record or not
     salary : how much does a standard investigation cost, default = -1
     fraud_label : default we have determined fraud at 1
 
@@ -55,29 +50,24 @@ def cost_investigation(data_point, predicted_label, salary=-1.0, fraud_label=1):
     cost of investigating for a label
 
     """
-    correct_label = data_point[-1]
     profit = 0
     loss = 0
-    if (correct_label == predicted_label) & (correct_label == fraud_label):
-        # we have found fraud correctly
-        # we are on the right way to get our investigation back
-        profit = 2
+    correct_label = data_point[-1]
+    if (decision == 1) & (correct_label == fraud_label):
+        # we found a fraud label so we investigated in the right way! We made money!
+        profit = 4
         loss = 0
-    elif (correct_label == predicted_label) & (correct_label != fraud_label):
-        # we did not find fraud in a case where we did not expect fraud
-        # so we will qualify this as breaking even
-        profit = 1
+    elif (decision == 1) & (correct_label != fraud_label):
+        # we did investigate a none fraud case so we lost only the salary
+        profit = 0
         loss = 0
-    elif (correct_label != predicted_label) & (correct_label == fraud_label):
-        # we found fraud where the algorithm actually predicted no fraud
-        # hmm strange, it is good that we investigated it!!
-        profit = 2
-        loss = 0
-    elif (correct_label != predicted_label) & (correct_label != fraud_label):
-        # we have predicted fraud but it was not fraud at all
-        # hmm we are on the wrong path
+    elif (decision == 0) & (correct_label == fraud_label):
+        # we missed some fraud this is bad!!!
         profit = 0
         loss = -1
+    elif (decision == 0) & (correct_label != fraud_label):
+        # we did not investigate no fraud
+        profit = 1
+        loss = 0
     cost = salary + profit + loss
     return cost
-
