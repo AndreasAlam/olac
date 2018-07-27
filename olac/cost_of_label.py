87780176a2ee931b1162061c4b9de3cc9d0a6120
bram2506 <bram2506@gmail.com>
2018-07-27 16:12:31 +0200

    Cost of label (#21)
    * added the powerpoint file format, .pptx to the gitingore
    
    * added .gifs to the gitingore just for safety
    
    * created new file for cost of a label and removed popping clusters module from init file
    
    * fixed issue with import on cluster_generators
    
    * notebook for investigation of the functions and implemented the functions in the file cost_of_label.py
    
    * removed the untitled empty notebooks from branch, improved functions which calculate the cost of investigating a data point or an array of datapoints
    
    * changed init so that it works with the cost calculation module
    
    * explanation added in notebook
    
    * notebook with Naivelabeler class implemented
    
    * cost functions updated so that they work in the naive labeler
    
    * the notebooks where the cost of a label is explained how the function works
    
    * notebook with the naivelabeler is implemented. and class implemented in development

--- olac/cost_of_label.py
+++ olac/cost_of_label.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-def cost_of_label(data, decision, data_type='point'):
+def cost_of_label(data, decision, data_type='array', salary=-1.5):
     """
     This function gives back the cost of obtaining a label from the incoming data. It will check if the classification
     of the points one wants to investigate is correct and assesses a profit or loss on each result.
@@ -14,6 +14,7 @@ def cost_of_label(data, decision, data_type='point'):
     data : can be either a data point or a array of data points.
     decision : bool or array of bools, investigate a point True or False
     data_type : how does the data come in, either 'array' or 'point' like.
+    salary : How much it costs to investigate a point
 
     Returns
     -------
@@ -21,23 +22,35 @@ def cost_of_label(data, decision, data_type='point'):
 
     """
     if data_type == 'point':
-        cost = cost_investigation(data, decision)
+        cost = cost_investigation(data, decision, salary)
         return cost
 
     elif data_type == 'array':
+        print('Labeler:\t evaluate the array of data in queue')
         # array like data input
-        df = pd.DataFrame(list(data))
-        df.columns = ['x', 'y', 'label']
+        df = pd.DataFrame(data)
+        df.columns = ['data']
         df['decision'] = decision
-        df['cost'] = df.apply(lambda x: cost_investigation(x[['x', 'y', 'label']], x['decision']), axis=1)
+        df_invest = df[df['decision'] == 1]
+        if len(df_invest) == 0:
+            print('Labeler:\t no investigation done')
+            df['cost'] = 0
+        else:
+            df['cost'] = df_invest.apply(lambda x: cost_investigation(x['data'],
+                                                                      x['decision'],
+                                                                      salary),
+                                         axis=1)
+            df['cost'].fillna(0, inplace=True)
+        print('Labeler:\t finished evaluate the array')
 
         return np.array(df['cost'])
 
 
-def cost_investigation(data_point, decision, salary=-1.0, fraud_label=1):
+def cost_investigation_eval(data_point, decision, salary=-1.0, fraud_label=1):
     """
     Function that looks at the data is there fraud or not compared to what the model predicts.
-    We want to obtain the labels, did we make a correct decission or not and the cost consequences are made
+    We want to obtain the labels, did we make a correct decission or not and the cost consequences are made. This
+    function can be used for the evaluation of the model
     Parameters
     ----------
     data_point : series or array like object that has as last entry the true label
@@ -52,7 +65,7 @@ def cost_investigation(data_point, decision, salary=-1.0, fraud_label=1):
     """
     profit = 0
     loss = 0
-    correct_label = data_point[-1]
+    correct_label = data_point.true_label
     if (decision == 1) & (correct_label == fraud_label):
         # we found a fraud label so we investigated in the right way! We made money!
         profit = 4
@@ -64,10 +77,56 @@ def cost_investigation(data_point, decision, salary=-1.0, fraud_label=1):
     elif (decision == 0) & (correct_label == fraud_label):
         # we missed some fraud this is bad!!!
         profit = 0
-        loss = -1
+        loss = -2
+        salary = 0
     elif (decision == 0) & (correct_label != fraud_label):
         # we did not investigate no fraud
-        profit = 1
+        profit = 0
+        loss = 0
+        salary = 0
+    cost = salary + profit + loss
+    return cost
+
+
+def cost_investigation(data_point, decision, salary, fraud_label=1):
+    """
+    When labeling one wants to keep track of their costs during the labeling in time because of a certain budget
+    Parameters
+    ----------
+    data_point : series or array like object that has as last entry the true label
+    decision : array or bool if we have investigated the record or not
+    salary : how much does a standard investigation cost, default = -1
+    fraud_label : default we have determined fraud at 1
+
+    Returns
+    -------
+    Cost of label in the loop
+    """
+    correct_label = data_point.true_label
+    if (decision == 1) & (correct_label == fraud_label):
+        # we found a fraud label so we investigated in the right way! We made money!
+        profit = 3
+        loss = 0
+    else:
+        # we investigated on non-fraud
+        profit = 0
         loss = 0
     cost = salary + profit + loss
     return cost
+
+
+def simple_decider(self, points, prob):
+    """
+    Simplest labeler available. It will generate a decision randomly whether to investigate or not.
+    Parameters
+    ----------
+    self :
+    points : data points from queue
+    prob : probability that a point is investigated
+
+    Returns
+    -------
+
+    """
+    n = len(points)
+    return np.random.binomial(1, prob, size=n)
